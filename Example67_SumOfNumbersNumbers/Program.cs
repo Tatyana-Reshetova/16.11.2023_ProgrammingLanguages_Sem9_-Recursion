a737779d028aa24d034a432d5b41d4b964879bb4
Tatyana_Reshetova <vlasenko2205@gmail.com>
2023-11-14 16:30:24 +0300

    Сумма чисел числа

---
+++ Example67_SumOfNumbersNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿/***Задача 67:** Напишите программу, 
+которая будет принимать на вход число и возвращать сумму его цифр.
+
+453 -> 12
+
+45 -> 9*/
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int m = int.Parse(ReadLine());
+
+WriteLine ($"Сумма чисел числа {m} равна {SumNumbers(m)}");
+
+int sum = 0;
+while(m > 0){
+    sum += m % 10; // <=> sum = sum + m % 10
+    m /= 10; // <=> m = m / 10
+}
+//WriteLine($"{sum}");
+
+int SumNumbers(int number){
+    if (number == 0)
+        return 0;
+    return number % 10 + SumNumbers(number / 10);
+}
